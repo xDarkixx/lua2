d49f3d3cb593cf2e60e11fca5c00d5d44eacedd7
xDarkixx <andygeiffes35@gmail.com>
2026-09-04 20:37:58 +0100

    Add Network Modern wrapper for RotaryCraft

---
+++ RotaryCraftNetwork_Modern.lua
@@ -0,0 +1,16 @@
+-- RotaryCraftNetwork_Modern.lua
+-- BULDACITY/2 network wrapper for RotaryCraftDashboard_Modern.lua.
+-- Minecraft 1.7.10 / RotaryCraft V33a / OpenComputers 1.8.10
+
+local Network=require("Network")
+local ok,mode=Network.startClient("RotaryCraft // Command Center",{
+  controller="RotaryCraftDashboard_Modern.lua",
+  mod="RotaryCraft",
+  version="V33a",
+  network=true
+})
+if not ok then
+  io.stderr:write("BULDACITY Network unavailable: "..tostring(mode).."\n")
+end
+
+dofile("RotaryCraftDashboard_Modern.lua")
