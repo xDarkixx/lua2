228939c8c99da7b80463c8f5c661978f5645d239
xDarkixx <andygeiffes35@gmail.com>
2026-09-04 20:27:25 +0100

    Move network client dependency to generic Network.lua

--- IndustrialCraft2_Network_Modern.lua
+++ IndustrialCraft2_Network_Modern.lua
@@ -1,4 +1,4 @@
--- Buldacity network wrapper for IndustrialCraft2_Modern.lua
-local ok,net=pcall(require,"BuldacityNetworkClient")
-if ok and net and net.start then pcall(net.start) end
+-- Network wrapper for IndustrialCraft2_Modern.lua
+local ok,net=pcall(require,"Network")
+if ok and net and net.startClient then pcall(net.startClient,"IndustrialCraft 2",{controller="IndustrialCraft2_Modern.lua"}) end
 dofile("/IndustrialCraft2_Modern.lua")
