db49b878d0d6063f14f6c419e38d1a2ac2bc493d
xDarkixx <andygeiffes35@gmail.com>
2026-09-06 18:22:06 +0100

    Fix non-destructive Modern Apps network launcher

--- BuldacityApps_Modern_Network.lua
+++ BuldacityApps_Modern_Network.lua
@@ -1,4 +1,7 @@
 -- BuldacityApps_Modern_Network.lua
 -- Non-destructive launcher for BuldacityApps_Modern.lua.
 -- The original Modern application remains unchanged.
-dofile("/home/clients/ModernLauncher.lua")
+package.preload["Network"]=function()
+  return require("clients.network.ModernNetworkCompat")
+end
+return dofile("/home/BuldacityApps_Modern.lua")
